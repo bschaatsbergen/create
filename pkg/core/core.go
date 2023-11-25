501dbae90d7c2f09c12b67d39eb9e4616c7a13a6
Bruno Schaatsbergen <git@bschaatsbergen.com>
2023-11-25 21:13:52 +0100

    chore: don't expose core funcs

--- pkg/core/core.go
+++ pkg/core/core.go
@@ -13,26 +13,26 @@ import (
 // CreateFile is the entry point to create a file with the given name and options
 func CreateFile(fileName string, flagStore model.Flagstore) {
 	// Check for file existence and confirmation
-	if !ConfirmOverwrite(fileName) {
+	if !confirmOverwrite(fileName) {
 		return
 	}
 
 	// Create necessary directories
-	CreateDirectories(fileName)
+	createDirectories(fileName)
 
 	// Create or overwrite the file
-	f := CreateOrUpdateFile(fileName, flagStore)
-	defer CloseFile(f) // Close the file when this function is done
+	f := createOrUpdateFile(fileName, flagStore)
+	defer closeFile(f) // Close the file when this function is done
 
 	// Set file mode
-	SetFileMode(f, os.FileMode(flagStore.Mode))
+	setFileMode(f, os.FileMode(flagStore.Mode))
 
 	// Write content to the file
-	WriteContentToFile(f, flagStore.Content)
+	writeContentToFile(f, flagStore.Content)
 }
 
-// ConfirmOverwrite prompts the user for confirmation before overwriting an existing file
-func ConfirmOverwrite(fileName string) bool {
+// confirmOverwrite prompts the user for confirmation before overwriting an existing file
+func confirmOverwrite(fileName string) bool {
 	if _, err := os.Stat(fileName); err == nil {
 		fmt.Printf("File '%s' already exists. Do you want to overwrite it? (yes/no): ", fileName)
 		scanner := bufio.NewScanner(os.Stdin)
@@ -47,8 +47,8 @@ func ConfirmOverwrite(fileName string) bool {
 	return true
 }
 
-// CreateDirectories creates necessary directories for the file
-func CreateDirectories(fileName string) {
+// createDirectories creates necessary directories for the file
+func createDirectories(fileName string) {
 	if err := os.MkdirAll(filepath.Dir(fileName), 0770); err != nil {
 		logrus.Panic(err)
 	}
@@ -59,8 +59,8 @@ func CreateDirectories(fileName string) {
 	}
 }
 
-// CreateOrUpdateFile creates a new file or overwrites an existing file
-func CreateOrUpdateFile(fileName string, flagStore model.Flagstore) *os.File {
+// createOrUpdateFile creates a new file or overwrites an existing file
+func createOrUpdateFile(fileName string, flagStore model.Flagstore) *os.File {
 	f, err := os.Create(fileName)
 	if err != nil {
 		logrus.Panic(err)
@@ -69,23 +69,23 @@ func CreateOrUpdateFile(fileName string, flagStore model.Flagstore) *os.File {
 	return f
 }
 
-// SetFileMode sets the mode of the file
-func SetFileMode(f *os.File, mode os.FileMode) {
+// setFileMode sets the mode of the file
+func setFileMode(f *os.File, mode os.FileMode) {
 	if err := f.Chmod(mode); err != nil {
 		logrus.Panic(err)
 	}
 	logrus.Debug("Changed file mode to: ", mode)
 }
 
-// WriteContentToFile writes content to the file
-func WriteContentToFile(f *os.File, content string) {
+// witeContentToFile writes content to the file
+func writeContentToFile(f *os.File, content string) {
 	if content != "" {
 		f.Write([]byte(fmt.Sprintf("%s\n", content)))
 		logrus.Debug("Wrote content to file: ", content)
 	}
 }
 
-// CloseFile closes the file
-func CloseFile(f *os.File) {
+// closeFile closes the file
+func closeFile(f *os.File) {
 	defer f.Close()
 }
